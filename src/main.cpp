c098ecd58798f33c89c7d80909ccbb1ddabbc370
Baraich <gurvirsingh.baraich@gmail.com>
2024-10-24 19:54:53 +0530

    simple cli, that has -v and -o

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,48 @@
 #include <iostream>
+#include "config.h"
 
-int main()
-{
-  std::cout << "Hello, World!" << std::endl;
-  return 0;
+static int usage(const char* arg0) {
+  std::cerr 
+    << "Usage: " << arg0 << " [OPTIONS] -o <file> <source>\n\n"
+    << "Options: \n"
+    << " -v           : Prints the Vane version number.\n"
+    << " -o <file>    : Specify the output file.\n";
+
+  return EXIT_FAILURE;
+}
+
+int main(int argc, char** argv) {
+  std::string outfile;
+  std::string sourcefile;
+
+  if (argc < 2) {
+    return usage(argv[0]);
+  }
+
+  for (int i = 1; i < argc; i++) {
+    std::string arg = argv[i];
+
+    if (arg == "-v") {
+      std::cout << VANE_VERSION_STRING << std::endl;
+    }
+
+    else if (arg == "-o" && argc > (i + 1)) {
+      i++;
+      outfile = argv[i];
+    }
+
+    else if (sourcefile.empty()) {
+      sourcefile = argv[i];
+    }
+
+    else {
+      return usage(argv[0]);
+    }
+  }
+
+  if (sourcefile.empty()) {
+    return usage(argv[0]);
+  }
+
+  return EXIT_SUCCESS;
 }
